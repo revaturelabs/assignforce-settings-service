b61fbedd2293bca4571e9821e70521d47ee66577
tjansen17 <tnj@17cosmicln.net>
2018-07-20 11:29:24 -0400

    Pom updated. Startup changed

--- src/main/java/com/revature/assignforce/SettingsServiceApplication.java
+++ src/main/java/com/revature/assignforce/SettingsServiceApplication.java
@@ -2,6 +2,7 @@ package com.revature.assignforce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
@@ -10,6 +11,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class SettingsServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SettingsServiceApplication.class, args);
+		new SpringApplicationBuilder(SettingsServiceApplication.class).run(args);
+
+//		SpringApplication.run(SettingsServiceApplication.class, args);
 	}
 }
